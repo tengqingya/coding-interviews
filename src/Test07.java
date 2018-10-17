3fb1695184d2912d440e7ba9ac601e98b471a8c1
tengqingya <tengqingya@yeah.net>
2018-10-17 15:29:25 +0800

    test07

--- src/Test07.java
+++ src/Test07.java
@@ -12,6 +12,70 @@ public class Test07 {
      * 用两个核实现一个队列。队列的声明如下，诸实现它的两个函数appendTail和deleteHead，
      * 分别完成在队列尾部插入结点和在队列头部删除结点的功能。
      */
+
+
+
+
+
+
+    public static Stack<Integer> sta1;
+    public static Stack<Integer> sta2;
+
+    /**
+     * 某部分可以省略!!!
+     * @param i
+     */
+    public static void enq(int i){
+        //s1为空,需要s2先返回s1
+        if(sta1.isEmpty()){
+            while( !sta2.isEmpty() ){
+                sta1.push(sta2.pop());
+            }
+        }
+        sta1.push(i);
+        //s1不为空直接入队
+    }
+    public static int deq(){
+        //s2为空，s1进入s2
+        if(sta2.isEmpty()){
+            while( !sta1.isEmpty() ){
+                sta2.push(sta1.pop());
+            }
+        }
+        return sta2.pop();
+        //s2不为空，直接出兑
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static class MList<T> {
         // 插入栈，只用于插入的数据
         private Stack<T> stack1 = new Stack<>();
