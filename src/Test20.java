b0eabeb2fa203cf49034784632a9d12228ca8d1a
tengqingya <tengqingya@yeah.net>
2019-03-01 12:22:41 +0800

    输入一个矩阵，按照从外向里以顺时针的顺序依次打印每一个数字

--- src/Test20.java
+++ src/Test20.java
@@ -1,3 +1,5 @@
+import static java.lang.System.out;
+
 /**
  * Author: 王俊超
  * Date: 2015-04-23
@@ -10,6 +12,76 @@ public class Test20 {
      *
      * @param numbers 输入的二维数组，二维数组必须是N*M的，否则分出错
      */
+
+
+
+
+    //x不变y++，y从yMin到yMax，xMin++，yMax--
+    //y不变x++,x从xMin到xMax
+    //x不变y--,y从上上步的yMax到yMin，xMax--,yMin++
+    //y不变x--,x从xMax到xMin,
+
+
+    public static void test20(int[][] numbers) {
+        int xMin=0,yMin=0,xMax=numbers.length-1,yMax=numbers.length-1;
+        while( xMin<=xMax || yMin<=yMax ){
+            f1(numbers,xMin,yMin,yMax);
+            xMin++;
+            f2(numbers,yMax,xMin,xMax);
+            yMax--;
+            f3(numbers,xMax,yMin,yMax);
+            xMax--;
+            f4(numbers,yMin,xMax,xMin);
+            yMin++;
+        }
+        return;
+    }
+
+    public static void f1(int[][] numbers,int xMin,int yMin,int yMax){
+        for(int i=yMin;i<=yMax;i++){
+            out.println(numbers[xMin][i]);
+        }
+    }
+    public static void f2(int[][] numbers,int yMax,int xMin,int xMax){
+        for(int i=xMin;i<=xMax;i++){
+            out.println(numbers[i][yMax]);
+        }
+    }
+    public static void f3(int[][] numbers,int xMax,int yMin,int yMax){
+        for(int i=yMax;i>=yMin;i--){
+            out.println(numbers[xMax][i]);
+        }
+    }
+    public static void f4(int[][] numbers,int yMin,int xMax,int xMin){
+        for(int i=xMax;i>=xMin;i--){
+            out.println(numbers[i][yMin]);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void printMatrixClockWisely(int[][] numbers) {
         // 输入的参数不能为空
         if (numbers == null) {
@@ -39,7 +111,7 @@ public class Test20 {
 
         // 输出环的上面一行，包括最中的那个数字
         for (int i = y; i <= cols - y - 1; i++) {
-            System.out.print(numbers[x][i] + " ");
+            out.print(numbers[x][i] + " ");
         }
 
         // 环的高度至少为2才会输出右边的一列
@@ -48,7 +120,7 @@ public class Test20 {
             // 因为右边那一列的最上面那一个已经被输出了，所以行呈从x+1开始，
             // 输出包括右边那列的最下面那个
             for (int i = x + 1; i <= rows - x - 1; i++) {
-                System.out.print(numbers[i][cols - y - 1] + " ");
+                out.print(numbers[i][cols - y - 1] + " ");
             }
         }
 
@@ -57,7 +129,7 @@ public class Test20 {
         if (rows - x - 1 > x && cols - 1 - y > y) {
             // 因为环的左下角的位置已经输出了，所以列号从cols-y-2开始
             for (int i = cols - y - 2; i >= y; i--) {
-                System.out.print(numbers[rows - 1 - x][i] + " ");
+                out.print(numbers[rows - 1 - x][i] + " ");
             }
         }
 
@@ -66,7 +138,7 @@ public class Test20 {
         if (cols - 1 - y > y && rows - 1 - x > x + 1) {
             // 因为最左边那一列的第一个和最后一个已经被输出了
             for (int i = rows - 1 - x - 1; i >= x + 1; i--) {
-                System.out.print(numbers[i][y] + " ");
+                out.print(numbers[i][y] + " ");
             }
         }
     }
@@ -80,7 +152,8 @@ public class Test20 {
                 {13, 12, 11, 10, 9},
         };
         printMatrixClockWisely(numbers);
-        System.out.println();
+        test20(numbers);
+        out.println();
 
         int[][] numbers2 = {
                 {1, 2, 3, 4, 5, 6, 7, 8},
@@ -91,21 +164,21 @@ public class Test20 {
 
         };
         printMatrixClockWisely(numbers2);
-        System.out.println();
+        out.println();
 
 
         int[][] numbers3 = {
                 {1, 2, 3, 4, 5, 6, 7, 8}
         };
         printMatrixClockWisely(numbers3);
-        System.out.println();
+        out.println();
 
         int[][] numbers4 = {
                 {1, 2, 3, 4, 5, 6, 7, 8},
                 {16, 15, 14, 13, 12, 11, 10, 9}
         };
         printMatrixClockWisely(numbers4);
-        System.out.println();
+        out.println();
 
 
         int[][] numbers5 = {
@@ -119,7 +192,7 @@ public class Test20 {
                 {8}
         };
         printMatrixClockWisely(numbers5);
-        System.out.println();
+        out.println();
 
         int[][] numbers6 = {
                 {0, 1},
@@ -130,9 +203,10 @@ public class Test20 {
                 {11, 6},
                 {10, 7},
                 {9, 8}
+
         };
         printMatrixClockWisely(numbers6);
-        System.out.println();
+        out.println();
 
 
         int[][] numbers7 = {
@@ -140,13 +214,13 @@ public class Test20 {
                 {4, 3}
         };
         printMatrixClockWisely(numbers7);
-        System.out.println();
+        out.println();
 
         int[][] numbers8 = {
                 {1}
         };
         printMatrixClockWisely(numbers8);
-        System.out.println();
+        out.println();
 
         // 0个元素的数组
         printMatrixClockWisely(new int[][]{{}});
